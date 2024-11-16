6572d468d2bd1d45f6dddebdbece13239ea22179
Adams contreras <adamscontreras18@gmail.com>
2024-11-16 10:54:09 -0300

    Fix Title

--- src/app/Components/InicioComponents/welcomeImg/WelcomeImg.tsx
+++ src/app/Components/InicioComponents/welcomeImg/WelcomeImg.tsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import styles from "./welcomeImg.module.css";
 import { RiArrowDownWideLine } from "react-icons/ri";
+import { useIsMobile } from "@/app/Elements/hooks";
 
 export const WelcomeImg = () => {
+  const isMobile = useIsMobile();
   return (
     <div className="flex flex-col items-center justify-center w-screen h-[calc(100vh-3rem)] bg-black relative z-0 top-12">
       <Image
@@ -18,14 +21,18 @@ export const WelcomeImg = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80 backdrop-blur-sm backdrop-brightness-50 z-0"></div>
 
         {/* Texto encima */}
-        <h1
-          className={`font-bold text-9xl text-foreground relative z-10 ${styles.title}`}
-        >
-          Bienvenido a Privacidad-Total
-        </h1>
-        <h2 className="text-foreground relative z-10 ">
-          Gestion de parches de seguridad ciber
-        </h2>
+        {!isMobile && (
+          <h1
+            className={`font-bold text-9xl text-foreground relative z-10 ${styles.title}`}
+          >
+            Bienvenido a <br />Total Privacy
+          </h1>
+        )}
+        {!isMobile && (
+          <h2 className="text-foreground relative z-10 ">
+            Gestion de parches de seguridad ciber
+          </h2>
+        )}
         <div className="  absolute bottom-0 left-0 w-full flex flex-col items-center justify-center z-10">
           <RiArrowDownWideLine className="text-foreground relative z-10 text-4xl" />
         </div>
