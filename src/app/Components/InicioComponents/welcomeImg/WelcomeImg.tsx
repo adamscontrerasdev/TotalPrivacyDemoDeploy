a8502578fd3a4baeb393458a3345dfb3d66f191a
Adams contreras <adamscontreras18@gmail.com>
2024-11-16 11:24:18 -0300

    fet bg LUZ

--- src/app/Components/InicioComponents/welcomeImg/WelcomeImg.tsx
+++ src/app/Components/InicioComponents/welcomeImg/WelcomeImg.tsx
@@ -18,22 +18,43 @@ export const WelcomeImg = () => {
       />
       <div className="flex flex-col items-center justify-center w-screen h-screen relative">
         {/* Fondo con blur */}
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80 backdrop-blur-sm backdrop-brightness-50 z-0"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black backdrop-blur-sm z-0"></div>
+
+        {/* Destellos que se cruzan hacia el centro */}
+        <div
+          className="absolute top-0 left-0 w-full h-full backdrop-blur-sm z-0"
+          style={{
+            background:
+              "radial-gradient(circle at bottom left, rgba(220, 38, 38, 0.6) 10%, transparent 70%)",
+            opacity: 0.2,
+          }}
+        ></div>
+
+        <div
+          className="absolute top-0 left-0 w-full h-full backdrop-blur-sm z-0"
+          style={{
+            background:
+              "radial-gradient(circle at bottom right, rgba(0, 123, 255, 0.6) 10%, transparent 70%)",
+            opacity: 0.3,
+          }}
+        ></div>
 
         {/* Texto encima */}
         {!isMobile && (
           <h1
             className={`font-bold text-9xl text-foreground relative z-10 ${styles.title}`}
           >
-            Bienvenido a <br />Total Privacy
+            Bienvenido a <br />
+            Total Privacy
           </h1>
         )}
         {!isMobile && (
           <h2 className="text-foreground relative z-10 ">
-            Gestion de parches de seguridad ciber
+            Gestión de parches de seguridad ciber
           </h2>
         )}
-        <div className="  absolute bottom-0 left-0 w-full flex flex-col items-center justify-center z-10">
+
+        <div className="absolute bottom-0 left-0 w-full flex flex-col items-center justify-center z-10">
           <RiArrowDownWideLine className="text-foreground relative z-10 text-4xl" />
         </div>
       </div>
